60fce0e78b05606725dd70b33284f86c32a83b44
imran2942 <imran2942@live.com>
2025-05-03 00:47:04 +0500

    9

--- main.py
+++ main.py
@@ -3,24 +3,23 @@
 num = 29
 
 # To take input from the user
-#num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-# define a flag variable
+# Define a flag variable
 flag = False
 
 if num == 0 or num == 1:
-    print(num, "is not a prime number")
+    print(f"{num} is not a prime number")
 elif num > 1:
-    # check for factors
+    # Check for factors
     for i in range(2, num):
-        if (num % i) == 0:
-            # if factor is found, set flag to True
+        if num % i == 0:
+            # If factor is found, set flag to True
             flag = True
-            # break out of loop
             break
 
-    # check if flag is True
+    # Check if flag is True
     if flag:
-        print(num, "is not a prime number")
+        print(f"{num} is not a prime number")
     else:
-        print(num, "is a prime number")
+        print(f"{num} is a prime number")
